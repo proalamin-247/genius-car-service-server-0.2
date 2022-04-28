79864513ccf43ccb4b35dc32c9100b12b86ccda4
proalamin-247 <pro.alamin247@gmail.com>
2022-04-28 21:52:13 +0600

    .env file added , multiple data and singale data load function added

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dmszk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const serviceCollection = client.db('geniusCar').collection('service');
+
+        // multiple get data
+        app.get('/service', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // singale get data
+        app.get('/service/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+        
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
     res.send('Runing Genius server')
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Listening to port:', port)
 })
